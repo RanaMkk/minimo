5d669fef055d7ecd05c7ed7e95e809a3b7461306
Rana Muhammed Ali <99143413+RanaMkk@users.noreply.github.com>
2023-09-25 13:39:21 +0300

    create Application DbContext

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace minimo.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
