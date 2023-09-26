064a9d7d32b6330d3a8baeb55ae4cf0b3566473f
Rana Muhammed Ali <99143413+RanaMkk@users.noreply.github.com>
2023-09-27 00:31:28 +0300

    Add project Apis

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using minimo.Models;
+using minimo.Services;
+using System.Threading.Tasks;
+using minimo.dtos;
+
+namespace minimo.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ProjectService _projectService;
+
+        public ProjectController(ProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddProject([FromBody] projectdto project)
+        {
+            try
+            {
+                var addedProject = await _projectService.AddProjectAsync(project);
+                return Ok(addedProject);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            try
+            {
+                var projects = await _projectService.GetAllProjectsAsync();
+                return Ok(projects);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet]
+        [Route("getById")]
+        public async Task<IActionResult> GetProject(int id) {
+            try
+            {
+                var project = await _projectService.GetProjectByID(id);
+                return Ok(project);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> UpdateProject(int id, [FromBody] projectdto updatedProject)
+        {
+            try
+            {
+                var updated = await _projectService.UpdateProjectAsync(id, updatedProject);
+                return Ok(updated);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            try
+            {
+                await _projectService.DeleteProjectAsync(id);
+                return Ok("Project deleted"); 
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Services/ProjectService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using minimo.Models;
+using minimo.Context;
+using System.Threading.Tasks;
+using minimo.dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Task = System.Threading.Tasks.Task;
+
+namespace minimo.Services
+{
+    public class ProjectService
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public ProjectService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //creating a new project
+        public async Task<Project> AddProjectAsync(projectdto projectdto)
+        {
+            var project = new Project
+            {
+                Name = projectdto.Name,
+                Description = projectdto.Description,
+                startdate = projectdto.startdate,
+                enddate = projectdto.enddate,
+                UserId = projectdto.UserId,
+                Status = projectdto.Status
+            };
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+            return project;
+        }
+        //listing all projects
+        public async Task<List<Project>> GetAllProjectsAsync()
+        {
+            var projects = await _context.Projects.ToListAsync();
+            return  projects;
+        }
+        // Show project details by ID
+        public async Task<Project?> GetProjectByID(int id)
+        {
+            var project = await _context.Projects.SingleOrDefaultAsync
+                (p => p.Id == id);
+            return project;
+        }
+        // Update an existing project details
+        public async Task<Project> UpdateProjectAsync(int projectId, projectdto updatedProject)
+        {
+            var existingProject = await _context.Projects.SingleOrDefaultAsync
+                (p => p.Id == projectId);
+
+            if (existingProject == null)
+            {
+                throw new Exception("Project not found");
+            }
+
+            // Update project details based on the updatedProject object
+            existingProject.Name = updatedProject.Name;
+            existingProject.Description = updatedProject.Description;
+            existingProject.startdate = updatedProject.startdate;
+            existingProject.enddate = updatedProject.enddate;
+            existingProject.Status = updatedProject.Status;
+
+            await _context.SaveChangesAsync();
+
+            return existingProject;
+        }
+        //Delete an existing project
+        public async Task DeleteProjectAsync(int projectId)
+        {
+            var projectToDelete = await _context.Projects.SingleOrDefaultAsync(p => p.Id == projectId);
+
+            if (projectToDelete == null)
+            {
+                throw new Exception("Project not found");
+            }
+
+            _context.Projects.Remove(projectToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
